ca8ee3d4261ee0686f8859f9e9960efaf8f11576
Dmitry_Kurch <dmitry.kurch@gmail.com>
2018-02-12 19:58:44 +0300

    added function text

--- Task1/src/Test.java
+++ Task1/src/Test.java
@@ -1,5 +1,10 @@
 public class Test {
+    static public String text ()
+    {
+        return "My name is Slim Shady";
+    }
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Testing function text: "+text());
     }
 }
